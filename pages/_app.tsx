c2ecf2bfdae6c55fc275862c1253fda186faf048
Larry Buckalew <larry.buckalew@gmail.com>
2025-01-18 19:36:48 -0500

    Add theme support with dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
+import { ThemeProvider } from '@/context/ThemeContext';
 import { AuthProvider } from '@/context/AuthContext';
 import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
