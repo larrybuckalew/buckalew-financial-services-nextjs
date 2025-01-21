10cafa0fa6362d1cf746a9f60371472bffbc543e
Larry Buckalew <larry.buckalew@gmail.com>
2025-01-21 10:49:41 -0500

    Add Sentry configuration and environment example (#25)
    * Add Sentry configuration and environment example
    
    * Add comprehensive error handling, testing, and frontend quote component

--- jest.config.js
+++ jest.config.js
@@ -3,20 +3,27 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
   },
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-    '^.+\\.mjs$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(uuid|xlsx|recharts|papaparse)/)'
-  ],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
     '!src/**/*.d.ts',
     '!src/pages/_app.tsx',
     '!src/pages/_document.tsx'
   ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/.next/'
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  }
 };
