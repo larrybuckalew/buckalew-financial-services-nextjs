c23cf26b1ce52727bc5253a164272a2041c5c1f0
Larry Buckalew <larry.buckalew@gmail.com>
2025-01-22 00:06:53 -0500

    Add Playwright configuration

---
+++ playwright.config.ts
@@ -0,0 +1,24 @@
+import { PlaywrightTestConfig, devices } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testDir: './e2e',
+  timeout: 30000,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  use: {
+    trace: 'on-first-retry',
+    baseURL: process.env.TEST_BASE_URL || 'http://localhost:3000',
+  },
+  projects: [
+    {
+      name: 'Desktop Chrome',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
+  ],
+};
+
+export default config;
